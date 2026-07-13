c5c3fdc8529aa2f89f7b70f0f91f80edfabed6e7
BarilyakNL <barilyak2502@gmail.com>
2026-07-13 21:28:37 +0300

    add classes

---
+++ classes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Unit
+{
+    // Закрытое поле здоровья
+    private float health;
+
+    public string Name { get; }
+
+    public float Health
+    {
+        get { return health; }
+    }
+       
+    public int Damage { get; }
+      
+    public float Armor { get; }
+
+       public Unit() : this("Unknown Unit")
+    {
+    }
+
+
+    public Unit(string name)
+    {
+        Name = name;
+        health = 100f;   
+        Damage = 5;
+        Armor = 0.6f;
+    }
+
+       public float GetRealHealth()
+    {
+        return Health * (1f + Armor);
+    }
+
+        public bool SetDamage(int value)
+    {
+        health -= value * Armor;
+
+        return health <= 0;
+    }
+}
